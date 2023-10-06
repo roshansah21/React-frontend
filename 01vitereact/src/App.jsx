3f98d40bbf5c9dfe79a23b74fe0dbf2255f73901
roshansah21 <10532sahroshan@gmail.com>
2023-10-06 11:46:30 +0545

    01ViteReact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Roshan from "./assets/roshan"
+function App() {
+  return (
+    <>
+       <Roshan />
+       <h1>Roshan</h1>
+       <h2>Roshan</h2>
+    </>
+
+  )
+}
+
+export default App
---
+++ 01vitereact/src/assets/roshan.jsx
@@ -0,0 +1,6 @@
+function Roshan(){
+    return(
+        <h1>React is Ready</h1>
+    )
+}
+export default Roshan;
